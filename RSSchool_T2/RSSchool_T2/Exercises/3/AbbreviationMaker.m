0f51d7da0c8c434e5891f5d512027afac74bfd65
Mark <macvik17@mail.ru>
2020-03-22 02:10:11 +0300

    feat: add solution to exercise3

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,29 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    BOOL deleteFlag = false;
+    NSMutableString *string = [NSMutableString stringWithString:a];
+    for (int i = 0; i < [string length]; i++) {
+        if (deleteFlag) {
+            i--;
+            deleteFlag = false;
+            
+        }
+        char temp = [string characterAtIndex:i];
+        if (temp >= 97 && temp <= 122) {
+            temp -= 32;
+            if (![b containsString: [NSString stringWithFormat:@"%c", temp]]) {
+                [string deleteCharactersInRange:NSMakeRange(i, 1)];
+                deleteFlag = true;
+            }
+        }
+        else if (![b containsString: [NSString stringWithFormat:@"%c", temp]])
+            return @"NO";
+    }
+    string = [NSMutableString stringWithString:[string uppercaseString]];
+    if ([string isEqualToString:b])
+        return @"YES";
+    else
+        return @"NO";
 }
 @end
