3ef689a0d02e70d0f342eb8356398c03835a6b39
Mark <macvik17@mail.ru>
2020-03-23 14:45:38 +0300

    feat: add solution to exercise6

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -2,6 +2,16 @@
 
 @implementation Blocks
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _blockA = ^(NSArray *array){};
+        _blockB = ^(Class class){};
+        _blockC = ^(NSObject *result){};
+    }
+    return self;
+}
 
 @end
 
--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,14 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    int beforePrevious = 0, previous = 1, current = 1;
+    while (current*previous < [number intValue]) {
+        beforePrevious = previous;
+        previous = current;
+        current = beforePrevious + previous;
+    }
+    if (current*previous == [number intValue])
+        return @[[NSNumber numberWithInt:previous],[NSNumber numberWithInt:current], [NSNumber numberWithInt:1]];
+    else return @[[NSNumber numberWithInt:previous],[NSNumber numberWithInt:current], [NSNumber numberWithInt:0]];
 }
 @end
